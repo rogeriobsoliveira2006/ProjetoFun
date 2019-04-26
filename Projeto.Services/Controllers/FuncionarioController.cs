95f08c894d655448ddda968c7c4f5c5d860ff192
rogeriobsoliveira2006 <rogeriobsoliveira2006@outlook.com>
2019-04-26 19:40:58 -0300

    Adicionado a classe controle no projeto - parte 2

---
+++ Projeto.Services/Controllers/FuncionarioController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Projeto.Services.Models.Request;
+using Projeto.Services.Models.Response;
+
+namespace Projeto.Services.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FuncionarioController : ControllerBase
+    {
+        [HttpPost("cadastrar")] //requisição do tipo POST //URL:api/[controller]/cadastrar
+        public HttpResponseMessage Cadastrar(FuncionarioCadastroRequest request)
+        {
+            var response = new FuncionarioCadastroResponse();
+
+            if (ModelState.IsValid)
+            {
+                response.Mensagem = $"Funcionário {request.Nome} | Cadastrado com sucesso !";
+                
+                //retornar STATUS de erro (HTTP 200)
+                return CreateResponse(HttpStatusCode.OK, response);
+            }
+            else
+            {
+                response.Mensagem = "Ocorreram erros de validação.";
+                
+                //retornar STATUS de erro (HTTP 400)
+                return Request.CreateResponse(HttpStatusCode.BadRequest, response);
+
+            }
+        }
+    }
+}
---
+++ Projeto.Services/Models/Request/FuncionarioCadastroRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projeto.Services.Models.Request
+{
+    public class FuncionarioCadastroRequest
+    {
+        [Required(ErrorMessage = "POR FAVOR, INFORME O NOME DO FUNCIONARIO !")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "POR FAVOR, INFORME O SALARIO DO FUNCIONARIO !")]
+        public decimal Salario { get; set; }
+
+        [Required(ErrorMessage = "POR FAVOR, INFORME A DATA DE ADMISSÃO DO FUNCIONARIO !")]
+        public DateTime DataAdmissao { get; set; }
+    }
+}
